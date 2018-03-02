321a055ab9b7d7544d0fdce6fc5ee8e52b1cc23d
mkawserm <mkawserm@gmail.com>
2018-03-02 13:08:08 +0600

    all qbpaths c++ method added

--- qbpaths.js
+++ qbpaths.js
@@ -12,3 +12,35 @@ QbPaths.prototype.documents = function() {
 QbPaths.prototype.download = function() {
     return this.i_qbpaths.download();
 };
+
+QbPaths.prototype.qbdocuments = function() {
+    return this.i_qbpaths.qbdocuments();
+};
+
+QbPaths.prototype.qbdownload = function() {
+    return this.i_qbpaths.qbdownload();
+};
+
+QbPaths.prototype.qbapps = function() {
+    return this.i_qbpaths.qbapps();
+};
+
+QbPaths.prototype.qbbackground = function() {
+    return this.i_qbpaths.qbbackground();
+};
+
+QbPaths.prototype.qblogo = function() {
+    return this.i_qbpaths.qblogo();
+};
+
+QbPaths.prototype.home = function() {
+    return this.i_qbpaths.home();
+};
+
+QbPaths.prototype.fontAwesome = function() {
+    return this.i_qbpaths.fontAwesome();
+};
+
+QbPaths.prototype.materialFont = function() {
+    return this.i_qbpaths.materialFont();
+};
